0fd4e8eeb59e1463e3c46ead57b9e4d3a2fcdbb3
Cat25016085 <alex2501608@gmail.com>
2025-03-10 03:00:20 +0800

    Merge remote-tracking branch 'origin/main'

--- 123.cpp
+++ 123.cpp
@@ -1,1 +1,19 @@
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> arr = {5, 2, 9, 1, 7};
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+/*
+Output:
+1 2 5 7 9
+*/
 
